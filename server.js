3a54603480c32cf7d0db82ed8476c8bcdaa26a89
Andrew_Schneer <aschneer738@gmail.com>
2015-04-18 01:34:51 -0400

    Implemented MongoDB server, nearly finished /sendLocation POST route.

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ var express = require("express");
 var bodyParser = require("body-parser");
 // For validating client POST content.
 var validator = require("validator");
+// Create MongoDB objects.
+var MongoClient = require("mongodb").MongoClient;
+var format = require("util").format;
 // Create Express app.
 var app = express();
+
+// Start MongoDB server.
+var mongoURI = (process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost/comp20ass03mapserver");
+var db = MongoClient.connect(mongoURI, function(err, dbConnection) {
+	db = dbConnection;
+});
 
 // Set the port for the server to
 // listen on.
@@ -19,14 +28,43 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Respond with...
 app.post("/sendLocation",function(req,res){
 	// Validate input.
-	if(true)
+	var input = req.body;
+	if(!validator.isAlphanumeric(input.login)
+		|| !validator.isFloat(input.lat)
+		|| !validator.isFloat(input.lng))
 	{
+		res.status(400);
+		res.json({"error":"Whoops, something is wrong with your data!"});
+	}
+	else
+	{
+		// Grab current time.
+
+		// THIS MIGHT BE GRABBING WRONG TIME,
+		// DOUBLE CHECK LATER.
+
+		t = new Date();
+		t = new Date(t.getTime());
+		t = t.toJSON();
+
+		// Prepare new data for insertion
+		// into database (upsert).
+		var newDoc1 = {login: input.login, lat: input.lat, lng: input.lng, created_at: t};
+
+		db.collection("locations", function(err,coll){
+//			coll.remove({},{},function(){
+				coll.update({login: {$eq: input.login}}, newDoc1, {upsert: true, w: 1}, function(err,result){
+					coll.find().toArray(function(err,docs){
+						res.status(200);
+						res.json(docs);
+					});
+				});
+//			});
+		});
+
+		//{"_id":"54e95ff46cca2a030048cf14","login":"mchow","lat":40.67693,"lng":117.23193,"created_at":"2015-02-22T05:12:24.596Z"}
 
 	}
-	res.send({"error":"Whoops, something is wrong with your data!"});
-	res.status(200);
-	res.send();
-
 });
 // Handle GET requests to "/location.json".
 // Respond with...
