1c5ef24656fa12ec6915ab39232982e89daf2bcf
Andrew_Schneer <aschneer738@gmail.com>
2015-04-24 12:18:17 -0400

    Created extra route for clearing database via client request.

--- server.js
+++ server.js
@@ -227,5 +227,44 @@ app.get("/",function(req,res){
 	});
 });
 
+// Route to clear database.
+app.get("/dbclear",function(req,res){
+	//Grab route query parameters.
+	var input = req.query;
+	if(input.pswd === "pizza")
+	{
+		db.collection("locations", function(err1,coll){
+			if(err1 === null)
+			{
+				coll.remove({},{},function(err2,arg2){
+					if(err2 === null)
+					{
+						res.set("Content-Type","text/html");
+						res.status(200);
+						res.send("Success: Database cleared.");
+					}
+					else
+					{
+						res.set("Content-Type","text/html");
+						res.status(500);
+						res.send("Failure: Couldn't remove from collection.");
+					}
+				});
+			}
+			else
+			{
+				res.set("Content-Type","text/html");
+				res.status(500);
+				res.send("Failure: Couldn't open database collection.");
+			}
+		});
+	}
+	else
+	{
+		res.set("Content-Type","text/html");
+		res.status(403);
+		res.send("Failure: Access denied.");
+	}
+
 // Listen for HTTP requests.
 app.listen(app.get("port"));
